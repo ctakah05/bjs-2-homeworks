db3de6712ea0a14816bb1312a5d55c0029f7002f
AleksMax <0.5ctakah@gmail.com>
2022-12-06 15:04:54 +0300

    array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,6 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+ result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i])
   // Ваш код
 
   return result; // boolean
@@ -8,7 +8,7 @@ function compareArrays(arr1, arr2) {
 
 function advancedFilter(arr) {
   let resultArr;
-
+  resultArr = arr.filter(number => number > 0).filter(number => number % 3 == 0).map(number => number * 10)
   // Ваш код
 
   return resultArr; // array
