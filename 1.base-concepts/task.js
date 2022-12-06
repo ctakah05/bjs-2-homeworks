a0c202f225257a952f61851261e3fd593e50a516
AleksMax <0.5ctakah@gmail.com>
2022-12-06 13:41:05 +0300

    test

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,17 @@
 "use strict";
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-
-  return arr; // array
+  arr = b**2-4*a*c;
+  if (arr < 0) {
+   arr=[];
+   return arr; 
+  } else if (arr === 0) {
+   arr=[-b/(2*a)];
+   return arr;
+  } else { 
+   arr=[Math.round((-b + Math.sqrt(b) )/(2*a)),Math.round((-b - Math.sqrt(b) )/(2*a))]
+   return arr; // array
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,59 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min=arr[0];
+  max=arr[0];
+  avg=0;
+  for (let i=0; i < arr.length; i++) {
+    avg += arr[i]
+    if (min > arr[i]) {
+      min=arr[i];
+    }
+    if (max < arr[i]) {
+      max=arr[i]
+    }
+  }
+  avg = (avg/arr.length).toFixed(2)
+  avg = Number(avg)
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum =0;
+    for (let i=0; i < arr.length; i++){
+     sum += arr[i]
+    }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  for (let i=0; i < arrOfArr.length; i++){
+  let sum = func(arrOfArr[i]);
+  if (sum > max){
+    max = sum
+  }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min= 0;
+  let max= 0;
+  avg=0;
+  for (let i=0; i < arr.length; i++) {
+    min=arr[0]
+    max=arr[0]
+    if (min > arr[i]) {
+      min=arr[i];
+    }
+    if (max < arr[i]) {
+      max=arr[i]
+    }
+  }
+  console.log(max, min)
+  return Math.abs(max-min)
 }
+
