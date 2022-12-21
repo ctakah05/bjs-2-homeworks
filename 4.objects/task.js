d6fe6d63cd58901b14f6f38f185ba27ef42585a4
AleksMax <0.5ctakah@gmail.com>
2022-12-21 13:37:04 +0300

    4(update)

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,39 +1,42 @@
-class Student {
-    constructor(name, gender, age) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-    }
-    setSubject(subjectName) {
-        this.subject = subjectName;
-        return this;
-    }
-    exclude(reason) {
-        delete this.subject;
-        delete this.marks;
-        this.excluded = reason;
-    }
-    addMark(mark) {
-        if (this.marks === undefined) {
-            this.marks = [mark];
-        } else {
-            this.marks.push(mark);
-        }
-    }
-    addMarks(...args) {
-        this.marks = Array.from(args);
-    }
-    getAverage() {
-        let sum = 0;
-        for (let i = 0; i < this.marks.length; i += 1) {
-            sum += this.marks[i];
-        }
-        return sum / this.marks.length;
+function Student(name, gender, age) {
+this.name = name;
+this.gender = gender;
+this.age = age;
+}
+
+Student.prototype.setSubject = function(subjectName) {
+ this.subject = subjectName;
+ return this;
+}
+
+Student.prototype.exclude = function(reason){
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
+
+Student.prototype.addMark = function(mark) {
+    if ( this.marks === undefined) {
+    this.marks = [mark];
+    } else {
+    this.marks.push(mark);
     }
 }
 
+Student.prototype.addMarks = function(...args) {
+    if ( this.marks === undefined) {
+        this.marks = Array.from(args);
+        } else {
+            this.marks.push(...args);
+        }
 
+}
 
-
-
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i += 1) {
+        sum += this.marks[i];
+    }
+    return sum / this.marks.length
+}
 // ваш код для остальных методов
