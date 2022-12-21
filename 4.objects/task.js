7ddffcbfc8b782615d882ac3368db21632c61319
AleksMax <0.5ctakah@gmail.com>
2022-12-21 12:29:40 +0300

    4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,14 +1,39 @@
-function Student(name, gender, age) {
-    // Ваш код
- return {
-     age,
-     gender,
-     name,
- }
+class Student {
+    constructor(name, gender, age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+    setSubject(subjectName) {
+        this.subject = subjectName;
+        return this;
+    }
+    exclude(reason) {
+        delete this.subject;
+        delete this.marks;
+        this.excluded = reason;
+    }
+    addMark(mark) {
+        if (this.marks === undefined) {
+            this.marks = [mark];
+        } else {
+            this.marks.push(mark);
+        }
+    }
+    addMarks(...args) {
+        this.marks = Array.from(args);
+    }
+    getAverage() {
+        let sum = 0;
+        for (let i = 0; i < this.marks.length; i += 1) {
+            sum += this.marks[i];
+        }
+        return sum / this.marks.length;
+    }
 }
 
-Student.prototype.setSubject = function (subjectName) {
- 
-}
+
+
+
 
 // ваш код для остальных методов
